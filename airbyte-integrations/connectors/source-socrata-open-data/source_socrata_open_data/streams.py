d1e45759ae4b9defc7f82ee6329d4b7eacc43ef6
Andrea Montes <mamontesp@gmail.com>
2022-11-22 20:56:49 -0500

    Adding pagination feature

--- airbyte-integrations/connectors/source-socrata-open-data/source_socrata_open_data/streams.py
+++ airbyte-integrations/connectors/source-socrata-open-data/source_socrata_open_data/streams.py
@@ -19,50 +19,33 @@ class SocrataOpenDataStream(HttpStream, ABC):
         self.offset = offset or self.DEFAULT_OFFSET
         super().__init__(**kwargs)
 
-    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
-        """
-        print(response)
+    def next_page_token(self) -> Optional[Mapping[str, Any]]:
         self.offset = self.limit + self.offset
         paging_dictionary = {"$offset": self.offset}
 
         print(f'offset {self.offset}')
         return paging_dictionary
-        """
-        return None
-
-    def request_params(self, stream_state=None, **kwargs):
-        
-        stream_state = stream_state or {}
-        #params = super().request_params(stream_state=stream_state, **kwargs)
-        params={"$offset": self.offset,
-                "$limit": self.limit}
-
-        return params
         
         #return None
+
+    def request_params(self, stream_state=None, next_page_token: Mapping[str, Any] = None, **kwargs):
+        
+        stream_state = stream_state or {}
+        params={"$offset": self.offset,
+                "$limit": self.limit}
+        print(f'next_page_token {next_page_token}')
+        if next_page_token:
+            params.update(next_page_token)
+        
+        return params
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
         TODO: Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        #response_json = response.json()
-  
-        #yield from response_json
+        response_json = response.json()
+        yield from response_json
 
     def read_records(
         self,
@@ -76,20 +59,18 @@ class SocrataOpenDataStream(HttpStream, ABC):
         #    return []
 
         try:
-            #yield from super().read_records(sync_mode, cursor_field, stream_slice, stream_state)
-            params = self.request_params()
             path = self.path()
-            print(f'domain {self.domain}')
-            full_path = f'{self.domain}/{path}.json'
-            print(f'full_path {full_path}')
-            print(f'path {path}')
-            response = requests.get(f'{self.domain}/{path}.json', params = params)
-            print(response.json())
-            if response.status_code == 200:
-                print(f'Status code {response.status_code}')
-            
-            print("Before return response")
-            yield from response.json()
+            response = requests.get(f'{self.domain}/{path}.json', params = {"$select": "count(*)"})
+            record_count = response.json()[0]['count']
+            for i in range(0, 12, self.limit):
+                params = self.request_params(next_page_token = self.next_page_token())
+                full_path = f'{self.domain}/{path}.json'
+                
+                response = requests.get(f'{self.domain}/{path}.json', params = params)
+                
+                if response.status_code == 200:
+                    self.logger.info(f'Status code {response.status_code}')
+                yield from response.json()
 
         except requests.exceptions.HTTPError as e:
             status_code = e.response.status_code
